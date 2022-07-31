87221a39a6858c4b51cd5d9fa9cdef0ee01b4bd1
cheng-hsiang <momonoshior@gmail.com>
2022-07-31 09:29:49 +0000

    remove  test script js

--- aaaa.js
+++
@@ -1,9 +0,0 @@
-const article_json = {
-    "article": {
-        "title": "123456",
-        "description": "6543241",
-        "body": "ggggg123456",
-        "tagList": ["reactjs"]
-    }
-}
-console.log(article_json.article.title)
